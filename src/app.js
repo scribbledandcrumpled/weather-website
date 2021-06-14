819ee2558cbc7deae0c46b66e783cc81a3915bd8
rax <scribbledandcrumpled@gmail.com>
2021-06-14 17:16:45 +0530

    rectified the misspelled word

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
-const port = proces.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 // Define paths for express config
